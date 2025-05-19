4c427c269d1b585076cb17d65439222a34e89c99
Vitalii Savitskii <visa@ecco.com>
2025-05-19 12:33:56 +0200

    Implement the schemas API

--- src/http/errors.rs
+++ src/http/errors.rs
@@ -1,6 +1,12 @@
+use actix_web::error::PayloadError;
+use anyhow::anyhow;
+use cedar_policy::entities_errors::EntitiesError;
 use std::fmt::{Debug, Display, Formatter};
+
 #[derive(Debug)]
 pub struct Error(anyhow::Error);
+
+pub type Result<T> = actix_web::Result<T, Error>;
 
 impl Error {
     pub fn new(msg: &'static str) -> Self {
@@ -11,13 +17,6 @@ impl Error {
         Self(err)
     }
 }
-
-impl From<anyhow::Error> for Error {
-    fn from(err: anyhow::Error) -> Self {
-        Self(err)
-    }
-}
-
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let err = &self.0;
@@ -25,10 +24,27 @@ impl Display for Error {
     }
 }
 
+impl From<anyhow::Error> for Error {
+    fn from(err: anyhow::Error) -> Self {
+        Self(err)
+    }
+}
+
+
 impl From<Error> for actix_web::Error {
     fn from(err: Error) -> Self {
         actix_web::error::ErrorInternalServerError(err)
     }
 }
 
-pub type Result<T> = actix_web::Result<T, Error>;
+impl From<EntitiesError> for Error {
+    fn from(err: EntitiesError) -> Self {
+        Self(anyhow!(err.to_string()))
+    }
+}
+
+impl From<PayloadError> for Error {
+    fn from(err: PayloadError) -> Self {
+        Self(anyhow!(err.to_string()))
+    }
+}
