b4eeb9e517d3671232823f9d10ef0b9fe12c07df
Vitalii Savitskii <visa@ecco.com>
2025-06-25 11:21:52 +0200

    Add error messages and check if kind is installed

--- src/services/backends/kubernetes/identity_repository/tests.rs
+++ src/services/backends/kubernetes/identity_repository/tests.rs
@@ -27,6 +27,12 @@ async fn get_kubeconfig() -> Result<Config> {
     let output = Command::new("kind")
         .args(&["get", "kubeconfig", "--name", "kind"])
         .output()?;
+    if !output.status.success() {
+        return Err(anyhow::anyhow!(
+            "Failed to get kubeconfig: {}",
+            String::from_utf8_lossy(&output.stderr)
+        ));
+    }
     let kubeconfig_string = String::from_utf8(output.stdout)?;
     info!("Kubeconfig used by the tests:\n{}", kubeconfig_string);
     let kubeconfig: Kubeconfig = serde_yml::from_str(&kubeconfig_string)?;
