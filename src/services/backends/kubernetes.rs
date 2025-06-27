8700bfde8bf6cc93dce51ff8907e941948422caa
Vitalii Savitskii <visa@ecco.com>
2025-06-27 10:25:20 +0200

    Revert "WIP"
    This reverts commit 8aef1e60c47f9be87b3f385115b82a125c601e87.

--- src/services/backends/kubernetes.rs
+++ src/services/backends/kubernetes.rs
@@ -1,53 +1,5 @@
-use std::collections::HashMap;
-use std::sync::Arc;
-use tokio::sync::RwLock;
-use crate::services::backends::base::Backend;
-use crate::services::backends::in_memory::InMemoryBackend;
-use crate::services::backends::kubernetes::schema_repository::KubernetesSchemaRepository;
-use crate::services::base::upsert_repository::{IdentityRepository, PrincipalAssociationRepository, PrincipalRepository, SchemaRepository};
-
 pub mod common;
 mod identity_repository;
 
 mod principal_repository;
 mod schema_repository;
-
-pub struct KubernetesBackend {
-    pub schemas_repository: Arc<SchemaRepository>,
-    pub entities_repository: Arc<PrincipalRepository>,
-    pub principal_association_repository: Arc<PrincipalAssociationRepository>,
-    pub identity_repository: Arc<IdentityRepository>,
-}
-
-impl KubernetesBackend {
-    pub fn new() -> Self {
-        let schemas_repository = KubernetesSchemaRepository::start()
-        let entities_repository = Arc::new(RwLock::new(HashMap::new()));
-        let principal_association_repository = Arc::new(RwLock::new(HashMap::new()));
-        let identity_repository = Arc::new(RwLock::new(HashMap::new()));
-        KubernetesBackend {
-            schemas_repository,
-            entities_repository,
-            principal_association_repository,
-            identity_repository,
-        }
-    }
-}
-
-impl Backend for KubernetesBackend {
-    fn get_schemas_repository(&self) -> Arc<SchemaRepository> {
-        Arc::clone(&self.schemas_repository)
-    }
-
-    fn get_entities_repository(&self) -> Arc<PrincipalRepository> {
-        Arc::clone(&self.entities_repository)
-    }
-
-    fn get_principal_association_repository(&self) -> Arc<PrincipalAssociationRepository> {
-        Arc::clone(&self.principal_association_repository)
-    }
-
-    fn get_identity_repository(&self) -> Arc<IdentityRepository> {
-        Arc::clone(&self.identity_repository)
-    }
-}
--- src/services/backends/kubernetes/schema_repository.rs
+++ src/services/backends/kubernetes/schema_repository.rs
@@ -69,30 +69,22 @@ struct SchemaConfigMap {
 }
 
 pub struct KubernetesSchemaRepository {
-    repository: Option<KubernetesRepository<SchemaConfigMap>>,
-    label_selector_key: Option<String>,
-    label_selector_value: Option<String>,
+    repository: KubernetesRepository<SchemaConfigMap>,
+    label_selector_key: String,
+    label_selector_value: String,
 }
 
 impl KubernetesSchemaRepository {
     #[allow(dead_code)] // Dead code is allowed here because this function is used in kubernetes
-    pub async fn new() -> Self {
-        KubernetesSchemaRepository {
-            None,
-            None,
-            None,
-        }
-    }
-    
-    #[allow(dead_code)] // Dead code is allowed here because this function is used in kubernetes
-    pub async fn start(mut self, config: RepositoryConfig) -> anyhow::Result<Self> {
+    pub async fn start(config: RepositoryConfig) -> anyhow::Result<Self> {
         let label_selector_key = config.label_selector_key.clone();
         let label_selector_value = config.label_selector_value.clone();
         let repository = KubernetesRepository::start(config, Arc::new(UpdateHandler)).await?;
-        self.repository = Some(repository);
-        self.label_selector_value = Some(label_selector_value);
-        self.label_selector_key = Some(label_selector_key);
-        Ok(self)
+        Ok(KubernetesSchemaRepository {
+            repository,
+            label_selector_key,
+            label_selector_value,
+        })
     }
 }
 
