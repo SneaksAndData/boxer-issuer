e9418725f4c2b47c9cc14249448f2781cbb9969d
Timon Viola <44016238+timonviola@users.noreply.github.com>
2026-06-08 15:53:18 +0200

    fix: rename endpoint url
    Apply suggestion from @s-vitaliy
    
    Co-authored-by: Vitalii Savitskii <visa@ecco.com>

--- src/http/health.rs
+++ src/http/health.rs
@@ -19,7 +19,7 @@ pub async fn get_health() -> actix_web::Result<String> {
     responses((status = OK, body = String)),
     responses((status = StatusCode::SERVICE_UNAVAILABLE, description = "Service not ready")),
 )]
-#[get("/probe")]
+#[get("/ready")]
 pub async fn get_health_probe(readiness_state: web::Data<Arc<AtomicBool>>) -> actix_web::Result<String> {
     if readiness_state.load(Ordering::Acquire) {
         return Ok("ok".into());
