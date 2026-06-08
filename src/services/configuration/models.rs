4b1f3b4424c1fd245ee3ea9af4908c696468bffe
Timon <tivi@ecco.com>
2026-06-08 13:02:47 +0200

    chore: fix ci

--- src/services/configuration/models.rs
+++ src/services/configuration/models.rs
@@ -41,6 +41,5 @@ pub struct AppSettings {
     pub init: InitializationSettings,
     pub backend: BackendSettings,
     pub opentelemetry: OpenTelemetrySettings,
-    pub opentelemetry: OpenTelemetrySettings,
     pub token_settings: TokenSettings,
 }
