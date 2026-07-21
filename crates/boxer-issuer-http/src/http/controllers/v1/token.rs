348ba14668d710a4403064d2f57f1eb6697926a9
Vitalii Savitskii <visa@ecco.com>
2026-07-21 14:20:50 +0200

    Implement an ability to issue v2 tokens form boxer-issuer

--- crates/boxer-issuer-http/src/http/controllers/v1/token.rs
+++ crates/boxer-issuer-http/src/http/controllers/v1/token.rs
@@ -1,6 +1,8 @@
+use actix_web::error::ErrorInternalServerError;
 use actix_web::get;
 use actix_web::web::{Data, Path, ReqData};
 use boxer_core::models::external_token::ExternalToken;
+use boxer_core::services::audit::chained::audit_event::AuditEvent;
 use boxer_core::services::token_service::internal_token_service::external_identity_validator_provider::external_identity_provider::ExternalIdentityProvider;
 use boxer_core::services::token_service::TokenService;
 use log::error;
@@ -17,11 +19,16 @@ pub async fn token(
     external_token: ReqData<ExternalToken>,
     token_service: Data<Arc<dyn TokenService>>,
     identity_provider: Path<String>,
+    audit_event: Data<AuditEvent>,
 ) -> actix_web::Result<String> {
     let ip = ExternalIdentityProvider::from(identity_provider.to_string());
+    let token_audit_event = audit_event
+        .into_inner()
+        .external_token_data()
+        .ok_or(ErrorInternalServerError("No token data in audit_event"))?;
     token_service
         .into_inner()
-        .issue_internal_token(ip, external_token.into_inner())
+        .issue_internal_token(ip, external_token.into_inner(), token_audit_event)
         .await
         .map_err(|err| {
             error!("Failed to issue internal token: {}", err);
