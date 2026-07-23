7804916bdf6c6ebccc5f3b33dff44b5c94716e21
Vitalii Savitskii <visa@ecco.com>
2026-07-23 15:16:48 +0200

    Bump boxer-core version

--- crates/boxer-issuer-http/src/http/controllers/v1/token.rs
+++ crates/boxer-issuer-http/src/http/controllers/v1/token.rs
@@ -2,6 +2,7 @@ use actix_web::error::ErrorInternalServerError;
 use actix_web::get;
 use actix_web::web::{Data, Path, ReqData};
 use boxer_core::models::external_token::ExternalToken;
+use boxer_core::services::audit::chained::audit_event::intermediate_audit_event::IntermediateAuditEvent;
 use boxer_core::services::audit::chained::audit_event::AuditEvent;
 use boxer_core::services::token_service::internal_token_service::external_identity_validator_provider::external_identity_provider::ExternalIdentityProvider;
 use boxer_core::services::token_service::TokenService;
@@ -22,10 +23,17 @@ pub async fn token(
     audit_event: ReqData<AuditEvent>,
 ) -> actix_web::Result<String> {
     let ip = ExternalIdentityProvider::from(identity_provider.to_string());
-    let token_audit_event = audit_event
-        .into_inner()
-        .external_token_data()
-        .ok_or(ErrorInternalServerError("No token data in audit_event"))?;
+    let audit_event = audit_event.into_inner();
+    let token_audit_event = match audit_event {
+        AuditEvent::Intermediate(IntermediateAuditEvent {
+            external_token: Some(token_event),
+            ..
+        }) => Ok(token_event),
+        _ => Err(ErrorInternalServerError(format!(
+            "Unexpected audit event: {:?}",
+            audit_event
+        ))),
+    }?;
     token_service
         .into_inner()
         .issue_internal_token(ip, external_token.into_inner(), token_audit_event)
